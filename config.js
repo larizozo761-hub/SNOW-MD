13b5031e90a90d9163e2ead4d64a493eac6e2f95
larizozo761-hub <larizozo761@gmail.com>
2025-08-01 15:52:11 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SNOW~MD~",
+SESSION_ID: process.env.SESSION_ID || "SNOW~MD~f4ZRCDbD#06iHC83Mc5Kn1KfqobBN7rLf6PFa6aW2j54iBbITah4",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -27,9 +27,9 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/rmcjwq.j
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "SNOW-MD",
+BOT_NAME: process.env.BOT_NAME || "LA REINE NOIR",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "POURQUOI AIMER QUANT ON PEUX DÉTESTE JON SNOW",
+STICKER_NAME: process.env.STICKER_NAME || "LA REINE NOIR",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "50949100359",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "18296826213",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "JON SNOW",
 // add bot owner name
